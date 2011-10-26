ac9d40facec392fdf210e9218648e639f5f2256e
kapcino <kapcino@gmail.com>
2011-10-26 20:09:36 +0800

    ex20

--- ex19.py
+++ ex19.py
@@ -4,8 +4,6 @@ def cheese_and_crackers(cheese_count, boxes_crackers):
     print 'Man that\'s enough for a party!'
     print 'Get a blanket.\n'
 
-
-
 print 'We can just give the function numers directly:'
 cheese_and_crackers(20, 30)
 
@@ -21,3 +19,9 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 print 'And we can combile the two, variables and math:'
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 100)
+
+def funbnanci(max):
+   print 'Fabonnanci %r' % max
+   print 'Len of %r is %d' % (max, len(max))
+
+funbnanci('40')
